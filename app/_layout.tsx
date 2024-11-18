b736dc8a6015c27471ee0b98361aa0a14b17189e
jbdura <bidurajohnson@gmail.com>
2024-11-18 23:13:46 +0300

    installed nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,8 +7,11 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import "../global.css";
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
+// noinspection JSIgnoredPromiseFromCall
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
